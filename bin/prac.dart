d4e3832295a64926bc4adf8c999ce3477c884fd8
Shyam7219 <shyamaptil0281@gamil.com>
2024-03-30 13:07:08 +0530

    first commit

---
+++ bin/prac.dart
@@ -0,0 +1,4 @@
+void main(){
+  print("fff");
+  print("ritesh");
+}
---
+++ test/prac_test.dart
@@ -0,0 +1,8 @@
+import 'package:prac/prac.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
